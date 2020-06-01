fae3265f3966c24601a143f5fab8d2910448c585
Eliot Kent Woodrich <ekwoodrich@gmail.com>
2020-05-31 20:34:32 -0700

    Add metadata to saved notes.

--- twith.js
+++ twith.js
@@ -1,5 +1,13 @@
 let noteList = [];
 let editing = true;
+let creating = true;
+
+function uuidv4() {
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+      var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
+      return v.toString(16);
+    });
+  }
 
 document.onkeydown = checkKey;
 
@@ -7,16 +15,14 @@ function checkKey(e) {
     var evtobj = window.event? event : e
 
     if (evtobj.key == 'n')  {
-        if(!editing) {
-            console.log('switching to new mode');
-            document.getElementById('main-note').style.display = 'none';
-            document.getElementById('main-edit').style.display = 'block';
-            document.getElementById('main-date').style.visibility = 'hidden';
+        console.log('switching to new mode');
+        document.getElementById('main-note').style.display = 'none';
+        document.getElementById('main-edit').style.display = 'block';
+        document.getElementById('main-date').style.visibility = 'hidden';
 
-            document.getElementById('main-edit').focus();
+        document.getElementById('main-edit').focus();
 
-            editing = true;
-        }
+        creating = true;
         event.preventDefault();
         return false;
     }
@@ -27,7 +33,7 @@ function checkKey(e) {
             document.getElementById('main-note').style.display = 'none';
             document.getElementById('main-edit').style.display = 'block';
             document.getElementById('main-date').style.visibility = 'hidden';
-            
+
             document.getElementById('main-edit').value = document.getElementById('main-note').innerHTML;
 
 
@@ -40,9 +46,12 @@ function checkKey(e) {
     }
      
     if (evtobj.key == 'Escape')  {
-        if (editing){
-
-            noteList.push(document.getElementById('main-edit').value);
+            
+            var dateobj = new Date(); 
+            if (creating){
+                noteList.push({'uuid': uuidv4(), created_on: dateobj.toISOString(), edited_on: dateobj.toISOString(),'body' : document.getElementById('main-edit').value});
+                creating=false;
+            }
             console.log(noteList);
 
             console.log('switching to view mode');
@@ -56,15 +65,14 @@ function checkKey(e) {
             document.getElementById('main-edit').style.display = 'none';
             document.getElementById('main-date').style.visibility = 'visible';
             editing = false;
-        }
+        
     }
     if (evtobj.key == 'ArrowRight' && evtobj.altKey)  {
         console.log('right')
         if (editing) {
-            noteList.push(document.getElementById('main').value);
+            noteList.push({'uuid': uuidv4(), 'body' : document.getElementById('main').value});
             document.getElementById('main-edit').value = '';
             console.log(noteList);
-    
         }
     }
 }
